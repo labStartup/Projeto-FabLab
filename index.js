fb2bccd9eee1bfb11232ae4b4ba7e79c11020c93
Romisson Oliveira <VIC0228675@aluno.firjansenaisesi.com.br>
2025-05-06 19:17:42 -0300

    Inserindo comentario no css

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector(".nav");
+const hamburger = document.querySelector('.hamburger')
+const nav = document.querySelector('.nav')
+const dropdownToggle = document.querySelector('.dropdown > a')
 
-hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+// Função direcionada ao menu "hamburger"
+hamburger.addEventListener('click', () => nav.classList.toggle('active'))
+
+// Dropdown no modo mmobile
+dropdownToggle.addEventListener('click', function(e) {
+  if (window.innerWidth <= 750) {
+    e.preventDefault()
+    this.parentElement.classList.toggle('active')
+  }
+})
